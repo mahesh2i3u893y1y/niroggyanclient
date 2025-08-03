6518705f8d13d0cee4a726d6bac9775f5f182636
maheshpaade <mahimahesh2943@gmail.com>
2025-08-03 11:18:29 +0530

    spinner

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const LoadingSpinner = () => {
   return (
-    <div className="flex justify-center items-center w-full h-full p-6">
+    <div className="flex justify-center items-center w-full h-[100vh] p-6">
       <div
         className={`animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-blue-500`}
       ></div>
